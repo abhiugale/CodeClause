2e50e2fa318f81b53f928e0ec1802c1794f46ca3
abhiugale <abhiugale2002@gmail.com>
2024-07-07 13:43:10 +0530

    background generator project completed

--- background-generator/src/App.js
+++ background-generator/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ColorPicker from './components/ColorPicker';
+import GradientTypeSelector from './components/GradientTypeSelector';
+import Preview from './components/Preview';
 import './App.css';
 
 function App() {
+  const [colors, setColors] = useState(['#ff0000', '#0000ff']);
+  const [gradientType, setGradientType] = useState('linear');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Background Generator</h1>
+      <ColorPicker colors={colors} setColors={setColors} />
+      <GradientTypeSelector gradientType={gradientType} setGradientType={setGradientType} />
+      <Preview colors={colors} gradientType={gradientType} />
     </div>
   );
 }
